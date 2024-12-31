73306dfbf567ec0d6862577f0525187116162ff8
harsh khare <153971247+harsh81r@users.noreply.github.com>
2024-12-31 18:50:09 +0530

    Update Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -7,31 +7,32 @@ import { Link } from 'react-router-dom';
 
 function Home() {
     return (
-<div className='bg-orange-300-950'>
+<div className='bg-neutral-500'>
         <div className="mx-auto w-full max-w-7xl ">
             <aside className="relative overflow-hidden text-black rounded-lg sm:mx-16 mx-2 sm:py-16">
                 <div className="relative z-10 max-w-screen-xl px-4  pb-20 pt-10 sm:py-24 mx-auto sm:px-6 lg:px-8">
                     <div className="max-w-xl sm:mt-1 mt-80 space-y-8 text-center sm:text-right sm:ml-auto">
+
+                    <img
+                            src="./myph.jpg"
+                            className=" h-12.4 rounded-3xl text-center"
+                            alt="Logo"
+                        />
+
                         <h2 className="text-4xl font-bold sm:text-5xl">
-                            Download Now
-                            <span className="hidden sm:block text-4xl">Lorem Ipsum</span>
+                            👋 Hii I'm Harsh
+
+                         <h3 className='font-bold mt-5 text-xs sm:text-4xl'>Proficient MERN Stack Developer specializing in designing and implementing robust, scalable web applications. Expertise in MongoDB, Express.js, React.js, and Node.js, with a strong focus on creating seamless user experiences and efficient, full-stack solutions for modern businesses</h3>
+
+                            <span className="hidden sm:block text-4xl"> Mern stack Developer</span>
                         </h2>
 
                         <Link
-                            className="inline-flex text-white items-center px-6 py-3 font-medium bg-orange-700 rounded-lg hover:opacity-75"
+                            className="inline-flex text-white items-center px-6 py-3 font-medium rounded-lg hover:opacity-75"
                             to="/"
                         >
-                            <svg
-                                fill="white"
-                                width="24"
-                                height="24"
-                                xmlns="http://www.w3.org/2000/svg"
-                                fillRule="evenodd"
-                                clipRule="evenodd"
-                            >
-                                <path d="M1.571 23.664l10.531-10.501 3.712 3.701-12.519 6.941c-.476.264-1.059.26-1.532-.011l-.192-.13zm9.469-11.56l-10.04 10.011v-20.022l10.04 10.011zm6.274-4.137l4.905 2.719c.482.268.781.77.781 1.314s-.299 1.046-.781 1.314l-5.039 2.793-4.015-4.003 4.149-4.137zm-15.854-7.534c.09-.087.191-.163.303-.227.473-.271 1.056-.275 1.532-.011l12.653 7.015-3.846 3.835-10.642-10.612z" />
-                            </svg>
-                            &nbsp; Download now
+                            
+                            &nbsp; 
                         </Link>
                     </div>
                 </div>
@@ -45,7 +46,7 @@ function Home() {
                 {/* <img className="sm:w-96 w-48" src="https://i.ibb.co/2M7rtLk/Remote1.png" alt="image2" /> */}
             </div>
 
-            <h1 className="text-center text-2xl sm:text-5xl py-10 font-medium">Let's Go</h1>
+            <h1 className="text-center text-2xl sm:text-5xl py-10 font-medium"></h1>
         </div>
         </div>
     );
