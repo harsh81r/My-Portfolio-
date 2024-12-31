ff8b96d0a6e8c6fcc53768d41f0a8b213466ebca
harsh khare <harsh7103763@gmail.com>
2024-12-31 17:31:11 +0530

    my portfolioe

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+// import React from 'react'
+// import './index.css'
+// import { Route, Routes } from 'react-router-dom'
+// import Layout from './Layout.js'
+// // import Header from './components/Header/Header.js'
+// // import Footer from './components/Footer/Footer.js'
+// import Home from './components/Home/Home.js'
+// import About from './components/About/About.js'
+// import Contact from './components/Contact/Contact.js'
+// import User from './components/Users/Users.js'
+// import Github, { githubInfoLoader } from './components/Github/Github.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// // const router = createBrowserRouter([
+// //   {
+// //     path: '/',
+// //     element: <Layout/>,
+// //     children: [
+// //       {
+// //         path: "",
+// //         element: <Home />
+// //       },
+// //       {
+// //         path: "about",
+// //         element: <About />
+// //       },
+// //       {
+// //         path: "contact",
+// //         element: <Contact />
+// //       }
+// //     ]
+// //   }
+// // ])
 
-export default App;
+
+
+// function App() {
+  
+//   return (
+
+
+// <Routes>
+// <Route path='' element={<Layout/>}></Route>
+// <Route path='/' element={<Home/>}></Route>
+// <Route path='about' element={<About/>}></Route>
+// <Route path='contact' element={<Contact/>}></Route>
+// <Route path='github' element={<Github/>}></Route>
+// <Route path="/User/:Userid" element={<User/>}></Route>
+
+// </Routes>
+
+    
+//   )
+// }
+
+// export default App
--- src/index.js
+++ src/index.js
@@ -1,17 +1,62 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+// import App from './App.css'
+import './index.css'
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+import Layout from './Layout.js'
+import Home from './components/Home/Home.js'
+import About from './components/About/About.js'
+import Contact from './components/Contact/Contact.js'
+import User from './components/Users/Users.js'
+import Github, { githubInfoLoader } from './components/Github/Github.js'
+import Groundwater from './components/Groundwater.js'
+
+import Button from './components/Button.js'
+
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <Layout/>,
+//     children: [
+//       {
+//         path: "",
+//         element: <Home />
+//       },
+//       {
+//         path: "about",
+//         element: <About />
+//       },
+//       {
+//         path: "contact",
+//         element: <Contact />
+//       }
+//     ]
+//   }
+// ])
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout />}>
+      <Route path='' element={<Home />} />
+      <Route path='about' element={<About />} />
+      <Route path='contact' element={<Contact />} />
+      <Route path='user/:userid' element={<User />} />
+      <Route 
+      loader={githubInfoLoader}
+      path='github' 
+      element={<Github />}
+       />
+       <Route path='/Groundwater' element={<Groundwater/>}></Route>
+    </Route>
+    
+  )
+)
+
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
